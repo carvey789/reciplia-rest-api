c6f3ae56286453a34d64f86335642d8bdd5d8b57
carvey789 <carvey789@gmail.com>
2024-06-26 17:57:07 +0700

    add: demo api

---
+++ src/data/recipes.ts
@@ -0,0 +1,18 @@
+export const dataRecipes = [
+  {
+    id: 0,
+    name: "Cake",
+  },
+  {
+    id: 1,
+    name: "Fried Rice",
+  },
+  {
+    id: 2,
+    name: "Fried Noodles",
+  },
+  {
+    id: 3,
+    name: "Bakso",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { dataRecipes } from "./data/recipes";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.json({
+    message: "Reciplia API",
+    recipes: "/recipes",
+  });
+});
 
-export default app
+app.get("/recipes", (c) => {
+  return c.json(dataRecipes);
+});
+
+app.get("/recipes/:id", (c) => {
+  const id = Number(c.req.param("id"));
+  const recipe = dataRecipes.find((recipe) => recipe.id === id);
+
+  if (!id) {
+    return c.json({
+      message: "There is no ID provided",
+    });
+  }
+
+  if (!recipe) {
+    return c.json({
+      message: "Recipe not found",
+    });
+  }
+
+  return c.json({
+    message: "Recipe found",
+    result: recipe,
+  });
+});
+
+export default app;
